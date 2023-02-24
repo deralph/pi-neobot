8b6b79769799c96922764f3b12a34f396f36d4f1
deralph <jraphael441@gmail.com>
2023-02-24 05:38:51 -0800

    added backend and frontend together

--- src/handlers/errorMIddleware.ts
+++ src/handlers/errorMIddleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, ErrorRequestHandler } from "express";
 
 const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
-  console.log("you have an error");
+  console.log("you have an error ");
   console.log(err);
 
   return res.status(err.status || 500).json({ err });
