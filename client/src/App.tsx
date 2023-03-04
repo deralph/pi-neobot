4b7575e7a3e4620069e872eae9ea62e61c5cff24
Funshyaj <104315470+Funshyaj@users.noreply.github.com>
2023-03-04 02:24:18 +0100

    trying to make app show on brower

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -49,17 +49,9 @@ interface WindowWithEnv extends Window {
 const _window: WindowWithEnv = window;
 const backendURL = _window.__ENV && _window.__ENV.backendURL;
 
-const axiosClient = axios.create({
-  baseURL: `${backendURL}`,
-  timeout: 20000,
-  withCredentials: true,
-});
-const config = {
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  },
-};
+const axiosClient = axios.create({ baseURL: `${backendURL}`, timeout: 20000, withCredentials: true});
+const config = {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}};
+
 export interface MyPaymentMetadata {};
 
 type PaymentCallbacks = {
